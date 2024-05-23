7485b62398b74dbb86da8161bb5145d850bb4e01
AllThaf <thafafadillah@gmail.com>
2024-05-23 17:06:01 +0700

    feat: add tas laptop

--- main.py
+++ main.py
@@ -2,23 +2,18 @@ from sel import TokpedScrap as sc
 import merek_all_item as mrk
 
 if __name__ == "__main__":
-    link_keyboard = "https://www.tokopedia.com/search?st=&q=keyboard&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&navsource="
-    merek_keyboard = mrk.merek_keyboard()
-    scrap_keyboard = sc(link_keyboard, merek_keyboard)
-    scrap_keyboard.scrap()
-    scrap_keyboard.save_data("keyboard.json")
-    scrap_keyboard.approximate("keyboard.json")
+    # link_keyboard = "https://www.tokopedia.com/search?st=&q=keyboard&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&navsource="
+    # scrap_keyboard = sc(link_keyboard, mrk.merek_keyboard(), "keyboard.json")
+    # scrap_keyboard.scrap()
 
-    link_mouse = "https://www.tokopedia.com/search?st=&q=mouse&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&navsource="
-    merek_mouse = mrk.merek_mouse()
-    scrap_mouse = sc(link_mouse, merek_mouse)
-    scrap_mouse.scrap()
-    scrap_mouse.save_data("mouse.json")
-    scrap_mouse.approximate("mouse.json")
+    # link_mouse = "https://www.tokopedia.com/search?st=&q=mouse&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&navsource="
+    # scrap_mouse = sc(link_mouse, mrk.merek_mouse(), "mouse.json")
+    # scrap_mouse.scrap()
 
-    link_headphone = "https://www.tokopedia.com/search?q=headphone&source=universe&st=product&navsource=home&srp_component_id=02.02.02.01"
-    merek_headphone = mrk.merek_headphone()
-    scrap_headphone = sc(link_headphone, merek_headphone)
-    scrap_headphone.scrap()
-    scrap_headphone.save_data("headphone.json")
-    scrap_headphone.approximate("headphone.json")
+    # link_headphone = "https://www.tokopedia.com/search?q=headphone&source=universe&st=product&navsource=home&srp_component_id=02.02.02.01"
+    # scrap_headphone = sc(link_headphone, mrk.merek_headphone(), "headphone.json")
+    # scrap_headphone.scrap()
+
+    link_tas_laptop = "https://www.tokopedia.com/search?st=&q=tas%20laptop&srp_component_id=02.01.00.00&srp_page_id=&srp_page_title=&navsource="
+    scrap_tas_laptop = sc(link_tas_laptop, mrk.merek_tas_laptop(), "tas_laptop.json")
+    scrap_tas_laptop.scrap()
